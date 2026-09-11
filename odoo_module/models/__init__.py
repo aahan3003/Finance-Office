50cb43f4512f9c74b1a1b5c867f2e5c43a744c6b
aahan3003 <aahansoni324@gmail.com>
2026-09-11 12:35:55 +0530

    Add Odoo module init file

---
+++ odoo_module/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class LoanMenu(models.Model):
+    """Placeholder for menu structure - actual menus are defined in XML"""
+    _name = 'loan.menu'
+    _description = 'Loan Management Menu'
